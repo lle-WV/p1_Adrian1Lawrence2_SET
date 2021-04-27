c5967f42ca377b0822a6a4a978c8a4aebbb25bb6
1935779 <1935779@01-19832.pusd.dom>
2021-04-27 09:04:49 -0700

    initial commit for placeholder classes, not final

---
+++ Q4 Project/src/Card.java
@@ -0,0 +1,12 @@
+
+public class Card {
+	private int value;
+	private String color;
+	private String shape;
+	public Card(int v, String c, String s) {
+		value = v;
+		color = c;
+		shape = s;
+	}
+	
+}
