6b16da40efe868e6b18b83e6e987c3cddfd1fbbd
D1234567890 <adrian.celena.1517@gmail.com>
2021-06-15 13:13:19 -0700

    Card Class

--- Q4 Project/src/Card.java
+++ Q4 Project/src/Card.java
@@ -1,12 +1,60 @@
 
 public class Card {
-	private int value;
-	private String color;
-	private String shape;
-	public Card(int v, String c, String s) {
-		value = v;
-		color = c;
-		shape = s;
+
+	// Each card possesses four attributes
+
+	private int shape;
+	private int color;
+	private int shading;
+	private int quantity;
+
+	private String fileName;
+
+	public Card(int shape, int color, int shading, int quantity) {
+
+		// Initialize each attribute
+
+		this.shape = shape;
+		this.color = color;
+		this.shading = shading;
+		this.quantity = quantity;
+
+		// Initialize the file name using the card's attributes
+
+		fileName = "" + shape + color + shading + quantity + ".jpg";
+
 	}
+
+	// Getters and setters
 	
+	public int getShape() {
+
+		return shape;
+
+	}
+
+	public int getColor() {
+
+		return color;
+
+	}
+
+	public int getShading() {
+
+		return shading;
+
+	}
+
+	public int getQuantity() {
+
+		return quantity;
+
+	}
+
+	public String getFileName() {
+
+		return fileName;
+
+	}
+
 }
